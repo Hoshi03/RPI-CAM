a44ae45362699846c0dcfbd84323d8db20c423ba
khy0e <dhy04029@gmail.com>
2024-11-25 17:12:07 +0900

    add

--- rpi_cam_app/src/main.cpp
+++ rpi_cam_app/src/main.cpp
@@ -1,7 +1,9 @@
 #include "main.hpp"
+#include "spdlog/spdlog.h"
 
 int main(int argc, char const *argv[])
 {
     /* code */
+    
     return 0;
 }
---
+++ rpi_cam_app/src/videoPipeline.hpp
@@ -0,0 +1,11 @@
+#ifndef VIDEOPIPELINE_HPP
+#define VIDEOPIPELINE_HPP
+
+#include <gst/gst.h>
+#include <string>
+
+class VideoPipeline{
+    //pipeline 클래스가 필요 없는듯..?
+};
+
+#endif VIDEOPIPELINE_HPP
